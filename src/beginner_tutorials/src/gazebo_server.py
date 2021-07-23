856924ca6c47354e1667fa7a9c46f8aee126fc5c
Bhaswanth A <bhaswanthayapilla@gmail.com>
2021-07-23 16:43:22 +0530

    Service practice 1

--- src/beginner_tutorials/src/gazebo_server.py
+++ src/beginner_tutorials/src/gazebo_server.py
@@ -5,12 +5,18 @@ from sensor_msgs.msg import LaserScan
 from beginner_tutorials.srv import gazebo_server, gazebo_serverResponse
 
 rospy.init_node("Subscriber", anonymous=True)
+ls_obj = LaserScan()
+response = gazebo_serverResponse()
 
 def callback(message):
-    print(message.ranges)
+    global ls_obj
+    ls_obj = message
 
 def func(request):
-    return gazebo_serverResponse(request)
+    global ls_obj
+    response.distance = ls_obj.ranges[request.direction]
+    return response
+    
 
 rospy.Subscriber('/scan', LaserScan, callback)
 
